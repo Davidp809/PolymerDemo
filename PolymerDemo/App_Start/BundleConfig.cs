cf61d39ef13c5a0718115ab5b9c2ed0fa51a8434
David Price <Davidp809@gmail.com>
2015-11-20 23:43:35 -0800

    Adding Fonts

--- PolymerDemo/App_Start/BundleConfig.cs
+++ PolymerDemo/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace PolymerDemo
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/webcomponents").Include(
+                        "~/Scripts/webcomponentsjs/webcomponents-lite.min.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
